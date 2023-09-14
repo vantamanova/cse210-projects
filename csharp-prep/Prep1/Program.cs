71ebfe045b2fadff1a25e6cc7c308cac73e3bd52
vantamanova <vantamanova@yandex.ru>
2023-09-14 10:42:34 +0400

    Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"You name is {lastname}, {name} {lastname}");
+
     }
 }
