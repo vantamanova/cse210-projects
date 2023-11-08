723b4a3b67253427667111fb7b3b7c0886c6d4ff
vantamanova <vantamanova@yandex.ru>
2023-11-08 18:03:16 +0400

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square shape1 = new Square("black", 2);
+        Rectangle shape2 = new Rectangle("blue", 1, 2);
+        Circle shape3 = new Circle("white", 2);
+
+
+        List<Shape> shapesList = new List<Shape>();
+        shapesList.Add(shape1);
+        shapesList.Add(shape2);
+        shapesList.Add(shape3);
+
+        foreach (Shape element in shapesList) {
+            string color = element.GetColor();
+            double area = element.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}. ");
+        }
+    
     }
 }
